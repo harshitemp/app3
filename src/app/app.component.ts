9e4a5338da487be1f38684fd89c6e7eda2266ff2
harshitemp <harshitemp28@gmail.com>
2024-09-09 13:55:16 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TwoWayBindingComponent } from "./two-way-binding/two-way-binding.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TwoWayBindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
